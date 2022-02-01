8722f5a188f362aeb0c04bab923ad3f531725103
Jordan DeBeer <jordan@jordandebeer.com>
2022-02-01 11:32:02 -0500

    wip: add wordle spoiler function (#2)
    Co-authored-by: johnpyp <johnpyp.dev@gmail.com>

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -5,12 +5,14 @@ pub mod meta;
 pub mod owner;
 pub mod schizo;
 pub mod uwu;
+pub mod wordle;
 
 use meta::*;
 use owner::*;
 use schizo::*;
 use uwu::*;
+use wordle::*;
 
 #[group]
-#[commands(ping, quit, schizo, uwu, set_moderator_role, set_prefix)]
+#[commands(ping, quit, schizo, uwu, wordle, set_moderator_role, set_prefix)]
 pub struct General;
